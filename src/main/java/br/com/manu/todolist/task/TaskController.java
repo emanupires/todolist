bf36abf4d7320259bb0ac6645652f84311569a37
Manu <emanuelepires2000@gmail.com>
2025-07-16 17:20:42 -0300

    Criação de tarefas, validação de usuário, criptografia de senha.

---
+++ src/main/java/br/com/manu/todolist/task/TaskController.java
@@ -0,0 +1,29 @@
+package br.com.manu.todolist.task;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+import java.util.UUID;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@RestController
+@RequestMapping("/tasks")
+public class TaskController {
+
+    @Autowired
+    private ITaskRepository taskRepository;
+
+    @PostMapping("/")
+    public TaskModel create(@RequestBody TaskModel taskModel, HttpServletRequest request) {
+        var idUser = request.getAttribute("idUser");
+        taskModel.setIdUser((UUID) idUser);
+        var task = this.taskRepository.save(taskModel);
+        return task;
+    }
+
+}
